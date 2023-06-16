12fcc05e99af43afcf2ef82826567da67aece961
Coraline Thaon <coraline.thaon@gmail.com>
2023-06-16 17:04:45 +0200

    Theme Beans + Thème enfant

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,8 +1,13 @@
 <?php
 
-// Action qui permet de charger des scripts dans notre thème
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles(){
-    // Chargement du style.css du thème parent BlankSlate
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+// Include Beans. Do not remove the line below.
+require_once( get_template_directory() . '/lib/init.php' );
+
+// Remove this action and callback function if you are not adding CSS in the style.css file.
+add_action( 'wp_enqueue_scripts', 'beans_child_enqueue_assets' );
+
+function beans_child_enqueue_assets() {
+
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+
 }
